f378c75ed37b506d2468285fbd8fc83db44fd61e
Никита Возвышаев <nvozvyshaev@astralinux.ru>
2022-10-28 18:01:03 +0300

    classes

--- DeLoger.py
+++ DeLoger.py
@@ -4,29 +4,32 @@
 from datetime import datetime
 
 class DeLog:
+    '''Объект класса DeLog представляет собой фунции логирования и дебага.
+    При создании экземпляра класса, он принимает в качестве аргументов параметры
+    логирования: имя лога, формат даты и форма записи лога в файл.
+    rew позволяет перезаписывать лог при каждом запуске программы, в которой он применен,
+    что позволяет отображать актуальную информацию по последней запущенной сессии.'''
 
     #Эта функция выполняется сразу же, при обращении к классу или создании его экземпляра
-    #param - принимает значение "rew", означающее перезапись файла лога
-    def __init__(self, name='NONAME', datetype='dt', param=None):
-        self.param = param
-        self.name = name
-        if self.param == 'rew':
-            with open(self.name+'.log', 'w') as file:
-                None
+    def __init__(self, name:str = 'NONAME' , datetype:str = 'dt', param:str = None):
+        self.__param = param
+        self.__name = name
+        if self.__param == 'rew':
+            with open(self.__name+'.log', 'w') as file:
+                pass
         if datetype == 'dt':
-            self.datetype = '%d.%m.%Y|%H:%M:%S   '
+            self.__datetype = '%d.%m.%Y|%H:%M:%S   '
         elif datetype == 'd':
-           self.datetype = '%d.%m.%Y   '
+           self.__datetype = '%d.%m.%Y   '
         elif datetype == 't':
-            self.datetype = '%H:%M:%S   '
+            self.__datetype = '%H:%M:%S   '
 
-    def loging(self, data):
-        datalog = data
-        with open(self.name+'.log', 'a') as file:
-            file.write(datetime.today().strftime(self.datetype)+str(datalog)+'\n')
+    def loging(self, data: str) -> str:
+        with open(self.__name+'.log', 'a') as file:
+            file.write(datetime.today().strftime(self.__datetype)+str(data)+'\n')
             return(data)
 
-    def debug(self, data):
+    def debug(self, data: str) -> str:
         print(data)
         return data
 
@@ -36,7 +39,8 @@ if __name__ == '__main__':
 #в другой код. __name__ определяет "имя" скрипта. __main__ основнвной скрипт, который запущен
 #Внизу приведен пример применения модуля
 
-    vlog = DeLog('a','logTimed','t')
+    print(DeLog.__doc__)
+    vlog = DeLog('logTimed','t')
 
     vlog.loging('good day, yes? ')
     print(vlog.debug(datetime.today()).strftime('%d.%m.%Y|%H:%M   '))
